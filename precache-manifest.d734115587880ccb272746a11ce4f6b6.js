56420487d3f8896a57c8f7e338fdad1000657de9
Mario Sanchez Carrion <mariosc@gmail.com>
2020-02-05 18:07:11 -0500

    Updates

---
+++ precache-manifest.d734115587880ccb272746a11ce4f6b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "d835c4a2630325d02206",
+    "url": "/react-todo/static/js/main.d835c4a2.chunk.js"
+  },
+  {
+    "revision": "4ff5cf781541bc3b0575",
+    "url": "/react-todo/static/js/1.4ff5cf78.chunk.js"
+  },
+  {
+    "revision": "d835c4a2630325d02206",
+    "url": "/react-todo/static/css/main.9034e0f8.chunk.css"
+  },
+  {
+    "revision": "ab5b3997c36751d7318f29c70b927517",
+    "url": "/react-todo/index.html"
+  }
+];
